937e4338a62bf005456f19481ed4f59e5cc0cfff
Metin Yarci <metin.yarci@aurea.com>
2019-07-18 15:25:40 +0300

    revision1

---
+++ directory1/directory2/AParentClass.cs
@@ -0,0 +1,16 @@
+namespace directory1.directory2
+
+public class AParentClass
+{
+    public void DoWork()
+    {
+    }
+
+    public class ALongClass
+	{
+	    public void DoInnerWork()
+	    {
+	    }
+	}
+
+}
